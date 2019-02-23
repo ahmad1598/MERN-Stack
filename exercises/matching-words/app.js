198ee093e68111b21253d13992f86d50dd7a2a38
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-02-22 23:48:40 -0700

    matching words problem solving

--- exercises/matching-words/app.js
+++ exercises/matching-words/app.js
@@ -1,5 +1,13 @@
 const matching = str => {
-    
+    let arr = str.split(' ')
+    let arrObj = {}
+    return arr.filter(word => { 
+        if(arrObj[word]){
+            return word
+        } else{
+            arrObj[word] = word
+        }
+    })
 }
 
 console.log(matching("Banh mi pull skateboard ipsum lorem, kombucha scenester banjo. Franzen mlkshk consequat, PBR&B lever lever listicle irony pop-up sriracha on the saliva. Shabby on chic eu iceland far next level far lever pull drinking the right vinegar fanny pack pull minim right chicharrones migas."))
