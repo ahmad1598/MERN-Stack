cb1b36c245cce3ac240426a1389a469ab75a474b
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-03-01 21:10:00 -0700

    adjacent

--- exercises/adjacent/app.js
+++ exercises/adjacent/app.js
@@ -1,5 +1,6 @@
-function combined(arr){
-    let result = ['','','']
+
+const combined = arr => {
+    let result = ["","",""]
     for(let i = 0; i < arr.length - 2; i++ ){
         if(arr[i].length + arr[i+1].length + arr[i+2].length > result[0].length + result[1].length + result[2].length){
             result.splice(0, 3 , arr [i], arr[i + 1], arr[i + 2])
@@ -8,7 +9,7 @@ function combined(arr){
     return result
 }
 
-console.log(combined(["this", "is", "an", "array"])) // Output:  ["is", "an", "array"]
-console.log(combined(["this", "is", "an", "and" , "array"])) // ["an","and","array"]
-console.log(combined(["solving","problem","is","fun"])) //[ 'solving', 'problem', 'is' ]
-console.log(combined(["coding","is","most","awesome","thing","ever"])) //[ 'most', 'awesome', 'thing' ]
+console.log(combined(["this", "is", "an", "array"])) 
+console.log(combined(["this", "is", "an", "and" , "array"])) 
+console.log(combined(["solving","problem","is","fun"]))
+console.log(combined(["coding","is","most","awesome","thing","ever"])) 
