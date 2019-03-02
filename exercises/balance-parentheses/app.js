55813c472f4e54cfcbec7662457fb83f6c5c810d
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-03-01 21:29:44 -0700

    parentheses

--- exercises/balance-parentheses/app.js
+++ exercises/balance-parentheses/app.js
@@ -1,33 +1,32 @@
 // Given a string including parentheses, write a function that returns true if 
 //every opening parentheses has a closing parentheses.
 
+// Also, every closing parentheses needs an opening partner before it.
+
 
 function balanceParentheses(str){
-    let openCount = 0;
-    let closeCount = 0;
+    let open = 0;
+    let close = 0;
     let arr = [...str]
-    let strObj = {}
-    if(arr[0] === ')'){
-        return false
-    } else{
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i] === '('){
-                openCount++
-                strObj[arr[i]] = arr[i]
-            } else{
-                closeCount++
-                strObj[arr[i]] = arr[i]
+
+    for(let each of arr){
+        if(each === '('){
+            open++
+        }else {
+            close++
+            if(open < close){
+                return false;
             }
         }
     }
-    
-    
-    // return (openCount === closeCount)
+    return (open === close)
 }
 
 console.log(balanceParentheses("()()")) // Output: true
 console.log(balanceParentheses(")()(")) // Output:  false
 console.log(balanceParentheses("())(")) // Output:  false
-// console.log(balanceParentheses("(((()))((((())))))")) // Output:  true
+console.log(balanceParentheses("(())")) // Output:  true
+console.log(balanceParentheses("()))")) // Output:  false
+console.log(balanceParentheses("(((()))((((())))))")) // Output:  true
 
 
